341f11f2b43480901c9db4ea42025a5975df9683
balintkaszas <bkaszas@ethz.ch>
2020-05-18 18:18:06 +0200

    Time dependent pert

--- src/functions/models/timoshenko/gen_file_timoshenko.m
+++ src/functions/models/timoshenko/gen_file_timoshenko.m
@@ -18,24 +18,25 @@ X = [x;xd].';
 
 A = [zeros(ndof),eye(ndof);-M\K,-M\C];
 Fnl = [zeros(ndof,1);-M\f];
-
-
-rhs = A*X.' + Fnl;
+omega = 0.3;
+Fphi = zeros(ndof_spv, 1);
+Fphi(end) = 1;
+syms t;
+rhs = A*X.' + Fnl + 1e-4*Fphi*sin(omega*t);
 %dy = @(t,y) odfunc(t, y, A, Fnn);
 
 
 % compute the gradient symbolically 
-syms t;
-Fnn = matlabFunction(rhs, 'vars', {t,X}, 'File', 'TIMOSHENKODE');
-%matlabFunction(dy, 'vars', {X}, );
+Fnn = matlabFunction(rhs, 'vars', {t,X}, 'File', 'TIMOSHENKODE_Pert');
+matlabFunction(dy, 'vars', {t, X} );
 
-
-X = [x;xd].';
-MassMtx = [eye(ndof), zeros(ndof); zeros(ndof), M];
-Alin = [zeros(ndof),eye(ndof);-K,-C];
-Fnl = [zeros(ndof,1);-f];
-syms t;
-rhs = Alin*X.' + Fnl;
-Fnn = matlabFunction(rhs, 'vars', {t,X}, 'File', 'TIMOSHENKODEWithMass');
-MassMtx = double(MassMtx);
-save('MMtx.mat', 'MassMtx');
+% 
+% X = [x;xd].';
+% MassMtx = [eye(ndof), zeros(ndof); zeros(ndof), M];
+% Alin = [zeros(ndof),eye(ndof);-K,-C];
+% Fnl = [zeros(ndof,1);-f];
+% syms t;
+% rhs = Alin*X.' + Fnl;
+% Fnn = matlabFunction(rhs, 'vars', {t,X}, 'File', 'TIMOSHENKODEWithMass');
+% MassMtx = double(MassMtx);
+% save('MMtx.mat', 'MassMtx');
