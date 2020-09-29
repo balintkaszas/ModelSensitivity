2e323e274b233309c0d7898c88caac73cd3ef0f0
balintkaszas <bkaszas@ethz.ch>
2020-09-29 17:28:43 +0200

    Fixed DuffingExample

--- DuffingExample.m
+++ DuffingExample.m
@@ -1,5 +1,6 @@
+addPath;
 %% Define the system
-dyref = @(t,x) d_duffing(t,x,0, false); %% specify derivative for the duffing system
+dyref = @(t,x) d_duffing(t,x); %% specify derivative for the duffing system
 
 timeSpan = [0, 2*pi]; %% time span is one period of the driving
 duff = DynSystem(dyref, 2, [1,1]); % define the DynSystem object, with both Delta being 1
@@ -20,10 +21,10 @@ xs = linspace(domain(1,1), domain(1,2), resolution(1));
 ys = linspace(domain(2,1), domain(2,2), resolution(2));
 
 colormap(jet)
-FF = repmat(reshape(ms, [250, 250]),1,1,3);
+FF = repmat(ms,1,1,3);
 FF=smooth3(FF,'gaussian',3);
 ms1 = FF(:,:,1);
-surf(xs,ys,ms1);shading interp; axis equal;axis tight;colorbar; xlabel('x'); ylabel('y')
+surf(xs,ys,ms);shading interp; axis equal;axis tight;colorbar; xlabel('x'); ylabel('y')
 view([0 0 1]); axis equal; axis tight; shading interp;camlight;
 set(gca,'colorscale', 'log');
 set(gca,'ZScale', 'log');
